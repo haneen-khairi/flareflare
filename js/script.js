bc67d87172df44af0ea0e2cd37d73aec063c765c
haneen-khairi <77403713+haneen-khairi@users.noreply.github.com>
2022-03-20 14:16:57 +0300

    edits

--- js/script.js
+++ js/script.js
@@ -108,24 +108,24 @@
     }
 
 })(jQuery);
-const log = document.querySelector('.event-log-contents');
-const reload = document.querySelector('#reload');
+// const log = document.querySelector('.event-log-contents');
+// const reload = document.querySelector('#reload');
 
-reload.addEventListener('click', () => {
-  log.textContent ='';
-  window.setTimeout(() => {
-      window.location.reload(true);
-  }, 200);
-});
+// reload.addEventListener('click', () => {
+//   log.textContent ='';
+//   window.setTimeout(() => {
+//       window.location.reload(true);
+//   }, 200);
+// });
 
-window.addEventListener('load', (event) => {
-    log.textContent = log.textContent + 'load\n';
-});
+// window.addEventListener('load', (event) => {
+//     log.textContent = log.textContent + 'load\n';
+// });
 
-document.addEventListener('readystatechange', (event) => {
-    log.textContent = log.textContent + `readystate: ${document.readyState}\n`;
-});
+// document.addEventListener('readystatechange', (event) => {
+//     log.textContent = log.textContent + `readystate: ${document.readyState}\n`;
+// });
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    log.textContent = log.textContent + `DOMContentLoaded\n`;
-});
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     log.textContent = log.textContent + `DOMContentLoaded\n`;
+// });
